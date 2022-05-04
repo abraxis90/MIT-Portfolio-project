5046f919759205637f5914a2d06fe83106d54d84
Erica Madebeykin <erica.madebeykina@gmail.com>
2022-05-04 21:48:43 +0400

    update

---
+++ website.js
@@ -0,0 +1,8 @@
+<script>
+$(".jumbotron").css({ height: $(window).height() + "px" });
+
+$(window).on("resize", function() {
+  $(".jumbotron").css({ height: $(window).height() + "px" });
+});
+
+</script>
